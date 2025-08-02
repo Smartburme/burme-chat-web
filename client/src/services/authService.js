6372e66366f2faa9a21ba009e6b45abb937023f9
burmeai.io <athuyrain@gmail.com>
2025-08-02 18:03:01 +0630

    Create authService.js

---
+++ client/src/services/authService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const API_URL = '/api/v1/auth';
+
+const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  if (response.data.token) {
+    localStorage.setItem('token', response.data.token);
+  }
+  return response.data;
+};
+
+const login = async (credentials) => {
+  const response = await axios.post(`${API_URL}/login`, credentials);
+  if (response.data.token) {
+    localStorage.setItem('token', response.data.token);
+  }
+  return response.data;
+};
+
+const logout = () => {
+  localStorage.removeItem('token');
+};
+
+export default {
+  register,
+  login,
+  logout
+};
