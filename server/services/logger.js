5b2f57cc9d023a1ae0bc7f534cb978d0da480442
burmeai.io <athuyrain@gmail.com>
2025-08-02 21:50:45 +0630

    Create logger.js

---
+++ server/services/logger.js
@@ -0,0 +1,23 @@
+// server/services/logger.js
+const winston = require('winston');
+const { format, transports } = winston;
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.json()
+  ),
+  transports: [
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/combined.log' }),
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        format.simple()
+      )
+    })
+  ]
+});
+
+module.exports = logger;
