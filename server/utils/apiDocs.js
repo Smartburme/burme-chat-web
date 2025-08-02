e28866dcb3f401dafada82b7acc1e52cee1f8812
burmeai.io <athuyrain@gmail.com>
2025-08-02 18:17:19 +0630

    Create apiDocs.js

---
+++ server/utils/apiDocs.js
@@ -0,0 +1,31 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Burme Chat API',
+      version: '1.0.0',
+      description: 'API documentation for Burme Chat application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000/api/v1',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+module.exports = specs;
