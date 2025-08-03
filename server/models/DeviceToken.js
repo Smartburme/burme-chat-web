4c39fb7aa41975c7a73cc6399696724175b7f170
burmeai.io <athuyrain@gmail.com>
2025-08-03 10:16:31 +0630

    Create DeviceToken.js

---
+++ server/models/DeviceToken.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const deviceTokenSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  platform: {
+    type: String,
+    enum: ['android', 'ios', 'web'],
+    required: true
+  },
+  lastActive: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('DeviceToken', deviceTokenSchema);
